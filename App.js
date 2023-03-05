8aad20963d660dc8722b9e3719bf89ba8e0c5674
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2023-03-05 21:46:47 +0200

    Commit expo

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  console.log('Maria');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! </Text>
+      <Text style={styles.mariaText}>Hello, Maria!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +20,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mariaText: {
+    color: 'pink',
   },
 });
