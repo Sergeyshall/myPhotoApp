7e6e413fcc97cca5747da786f72693ea3a4fc2bf
MariaDekhtiaruk <109179342+MariaDekhtiaruk@users.noreply.github.com>
2023-04-05 09:29:21 +0300

    add camera code

--- screens/CreatePostsScreen.js
+++ screens/CreatePostsScreen.js
@@ -1,5 +1,7 @@
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import { Camera } from 'expo-camera';
 import {
   StyleSheet,
   Text,
@@ -7,58 +9,87 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-import imgForest from '../Img/Forest.jpg';
-export default CreatePostScreen = () => {
+
+export default CreatePostScreen = ({ navigation }) => {
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState('');
+  const takePhoto = async () => {
+    const photo = await camera.takePictureAsync();
+    setPhoto(photo.uri);
+    console.log('cam', photo.uri);
+  };
+
+  const sendPhoto = () => {
+    navigation.navigate('Posts');
+  };
+
   return (
-    <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
-      <Ionicons name="trash" color="white" />
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <Camera style={styles.camera} ref={setCamera}>
+        {photo && (
+          <View style={styles.photoContainer}>
+            <Image
+              source={{ uri: photo }}
+              style={{ width: 200, height: 200, borderRadius: 10 }}
+            />
+          </View>
+        )}
+        <TouchableOpacity
+          onPress={takePhoto}
+          style={styles.snapContainer}
+        >
+          <Text style={styles.snap}>Use camera</Text>
+        </TouchableOpacity>
+      </Camera>
+      <Text>Post your photo</Text>
+      <TouchableOpacity onPress={sendPhoto} style={styles.sentBtn}>
+        <Text style={styles.snap}>Publish</Text>
+      </TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
+        <Ionicons name="trash" color="white" />
+      </TouchableOpacity>
+    </View>
   );
 };
-
-// const Tabs = createBottomTabNavigator();
-// const DeleteButton = () => {
-//   <>
-//     <View style={{ ...styles.btn, backgroundColor: 'gray' }}>
-//       <Ionicons name={iconName} size={size} color={colorIcon} />
-//     </View>
-//   </>;
-// };
-
-// export default CreatePostsScreen = () => {
-//   return (
-//     <>
-//       <Tabs.Navigator>
-//         <Tabs.Screen
-//           name="CreatePost"
-//           component={() => (
-//             <View style={styles.container}>
-//               <Text>Create Post</Text>
-//             </View>
-//           )}
-//           options={{
-//             tabBarIcon: ({ name, color, size }) => (
-//               <Ionicons name="trash" color={'#F6F6F6'} size={size} />
-//             ),
-//           }}
-//           listeners={{
-//             tabPress: (e) => {
-//               // Prevent default action
-//               e.preventDefault();
-//               alert('Home');
-//             },
-//           }}
-//         />
-//       </Tabs.Navigator>
-//     </>
-//   );
-// };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+  },
+
+  camera: {
+    height: '50%',
+    marginHorizontal: 16,
+    marginTop: 32,
     justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+
+  snap: {
+    color: 'white',
+    fontFamily: 'Roboto-Regular',
+    fontSize: 16,
+    lineHeight: 19,
+  },
+
+  snapContainer: {
+    marginTop: 200,
+    width: 70,
+    height: 70,
+    backgroundColor: 'gray',
+    borderRadius: '50',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  photoContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
   },
   btn: {
     marginLeft: 16,
@@ -73,10 +104,13 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     padding: 10,
   },
-  btnText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    lineHeight: 19,
-    fontFamily: 'Roboto-Regular',
+  sentBtn: {
+    backgroundColor: 'gray',
+    width: 343,
+    height: 40,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
   },
 });
